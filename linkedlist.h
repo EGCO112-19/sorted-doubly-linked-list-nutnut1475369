104ec314ae0527f7acfe04044834b82c858e8268
ณัฐพล มนุโศภิษฐ์ <nutnut1475369@hotmail.com>
2021-02-19 10:37:33 +0000

    finalแบบสั้นๆ

--- linkedlist.h
+++ linkedlist.h
@@ -93,19 +93,16 @@ int deletes( LLPtr *sPtr, int value )
       } // end while
 
       // delete node at currentPtr
-      if ( currentPtr != NULL && currentPtr->nextPtr != NULL) { 
-         tempPtr = currentPtr;
-         previousPtr->nextPtr = currentPtr->nextPtr;
-         currentPtr->nextPtr->prevPtr = previousPtr;
-         free( tempPtr );
-         return value;
-      } // end if
       if ( currentPtr != NULL ) { 
          tempPtr = currentPtr;
          previousPtr->nextPtr = currentPtr->nextPtr;
+         if(currentPtr->nextPtr != NULL){
+          currentPtr->nextPtr->prevPtr = previousPtr;
+         }
          free( tempPtr );
          return value;
       } // end if
+
    } // end else
 
    return '\0';
